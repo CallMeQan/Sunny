2ba418856925609230b35d567c1ee4afad9f59a4
CallMeRy4n <minhquan99kk@gmail.com>
2022-01-31 21:42:56 +0700

    Update ChannelSetCommand.java logger and message cuz i forgot about it

--- src/main/java/com/atelier/sunny/commands/sub/ChannelSetCommand.java
+++ src/main/java/com/atelier/sunny/commands/sub/ChannelSetCommand.java
@@ -4,13 +4,18 @@ import com.atelier.sunny.models.GuildDocument;
 import com.atelier.sunny.utils.DatabaseUtils;
 import net.dv8tion.jda.api.entities.GuildChannel;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ChannelSetCommand {
+    private final Logger logger = LoggerFactory.getLogger(ChannelSetCommand.class);
+
     public void run(SlashCommandEvent event) {
         GuildChannel channel = event.getOption("channel").getAsGuildChannel();
         GuildDocument.convertDocument(DatabaseUtils.getDocument("guildID", event.getGuild().getId()))
                 .setChannelID(channel.getId())
                 .update();
-        event.reply(channel.getId()).queue();
+        event.reply("Succeed changed channel to " + channel.getAsMention()).queue();
+        logger.info("\""+event.getGuild().getName()+"\" had changed channel id to " + channel.getId());
     }
 }
