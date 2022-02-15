4bee2e25aeb07d3a1a66f85c2aecb181f11af9f2
CallMeRy4n <minhquan99kk@gmail.com>
2022-02-15 18:00:21 +0700

    Add logger for URLUtils

--- src/main/java/com/atelier/sunny/utils/URLUtils.java
+++ src/main/java/com/atelier/sunny/utils/URLUtils.java
@@ -1,8 +1,13 @@
 package com.atelier.sunny.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.net.URL;
 
 public class URLUtils {
+    private static final Logger logger = LoggerFactory.getLogger(URLUtils.class);
+
     public static boolean isValid(String url)
     {
         /* Try creating a valid URL */
@@ -13,7 +18,7 @@ public class URLUtils {
         // If there was an Exception
         // while creating URL object
         catch (Exception e) {
-
+            logger.error("Unknown link: "+ url, e.getCause());
             return false;
         }
     }
